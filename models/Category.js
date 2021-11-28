0fa9c003ac0b75c183b715c3c658d62acda18f7d
ZBreezie <zpmc55@yahoo.com>
2021-11-27 21:02:21 -0500

    defined all models and associations

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,25 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+     
+    // define an id column
+    id: {
+      // as integer
+      type: DataTypes.INTEGER,
+      // doesnt allow null values
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+     
+    category_name: {
+      // as string
+      type: DataTypes.STRING,
+      // doesnt allow null values
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,53 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      // as integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true
+    },
+    product_name: {
+      // as string
+      type: DataTypes.STRING,
+      // doesn't allow null values
+      allowNull: false,
+    },
+    price: {
+      // as decimal
+      type: DataTypes.DECIMAL,
+      // doesn't allow null values
+      allowNull: false,
+      // validates that the value is a decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      // as integer
+      type: DataTypes.INTEGER,
+      // doesnt allow null values
+      allowNull: false,
+      // sets default value to 10
+      defaultValue: 10,
+      // validates value as numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      // as integer
+      type: DataTypes.INTEGER,
+      // references category models id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
